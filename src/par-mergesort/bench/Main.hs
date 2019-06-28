f8507a43c48e98893320d1457f55959257d9e858
Sam Westrick <shwestrick@gmail.com>
2019-06-28 17:49:54 -0400

    tweaks to try par mergesort experiment

--- src/par-mergesort/bench/Main.hs
+++ src/par-mergesort/bench/Main.hs
@@ -5,8 +5,14 @@
 module Main where
 
 import           Data.Int
+import           Data.Word
+import           Data.Bits
 import qualified Data.Vector.Storable           as SV
 import qualified Data.Vector.Storable.Mutable   as SVM
+
+-- import           Data.Time.Clock
+
+-- import qualified Control.DeepSeq
 
 import           Criterion
 import           Criterion.Main
@@ -19,15 +25,52 @@ import qualified Control.Par.ST.StorableVec2    as V
 
 -- TODO: Add vector-algortihms benchmarks.
 
+{-
+main :: IO ()
+main =
+  do
+    putStrLn $ "warming up"
+    warmup 0 5 input
+    putStrLn $ "running experiments"
+    putStrLn ""
+    runMany 0 10 input
+  where
+    input = SV.map hash $ SV.fromList $ [0 .. (10^(8 :: Int32) - 1)]
+
+runMany :: Int -> Int -> SV.Vector Int32 -> IO ()
+runMany i n input =
+  if i == n then return () else do
+    putStrLn $ "==== run " ++ show i ++ " ===="
+    t0 <- getCurrentTime
+    let result = sortPV 10000 10000 CSort HSMerge input
+    putStrLn $ (show $ result SV.! 0) ++ " " ++ (show $ result SV.! 42)
+    t1 <- getCurrentTime
+    putStrLn $ "wall " ++ (show $ diffUTCTime t1 t0)
+    putStrLn ""
+    runMany (i+1) n input
+
+warmup :: Int -> Int -> SV.Vector Int32 -> IO ()
+warmup i n input =
+  if i == n then return () else do
+    t0 <- getCurrentTime
+    let result = sortPV 10000 10000 CSort HSMerge input
+    putStrLn $ (show $ result SV.! 0) ++ " " ++ (show $ result SV.! 42)
+    t1 <- getCurrentTime
+    putStrLn $ (show $ diffUTCTime t1 t0)
+    warmup (i+1) n input
+-}
+
 main :: IO ()
 main = defaultMain
-  [ env (return $ SV.fromList $ reverse [0 .. 10^(6 :: Int32)]) $ \(vec :: SV.Vector Int32) ->
+  [ env (return $ SV.map hash $ SV.fromList $ inputRange) $ \(vec :: SV.Vector Int32) ->
       bgroup "sorting benchmarks" $ concat $
-        flip map [CSort, VAMSort, VAISort] $ \ssMeth ->
-          flip map [CMerge, HSMerge] $ \smMeth ->
-            -- TODO(osa): Thresholds should be varying?
-            mkBench 100 100 ssMeth smMeth vec
+      flip map [CSort] $ \ssMeth ->
+      flip map [HSMerge] $ \smMeth ->
+      -- TODO(osa): Thresholds should be varying?
+      mkBench 10000 10000 ssMeth smMeth vec
   ]
+  where
+    inputRange = [0 .. (10^(8 :: Int32) - 1)]
 
 mkBench :: Int -> Int -> SSort -> SMerge -> SV.Vector Int32 -> Benchmark
 mkBench ssThres smThres ssMeth smMeth vec =
@@ -65,3 +108,19 @@ sortPV' ssThres smThres ssMeth smMeth vec = do
 --    SS.put (STTup2 (SFlp vec) (SFlp right))
     V.installL vec
     mergeSort_int32 ssThres smThres ssMeth smMeth
+
+hashWord :: Word64 -> Word64
+hashWord w = v8 where
+  v1 = w * 3935559000370003845 + 2691343689449507681
+  v2 = v1 `xor` (v1 `shiftR` 21)
+  v3 = v2 `xor` (v2 `shiftL` 37)
+  v4 = v3 `xor` (v3 `shiftR` 4)
+  v5 = v4 * 4768777513237032717
+  v6 = v5 `xor` (v5 `shiftL` 20)
+  v7 = v6 `xor` (v6 `shiftR` 41)
+  v8 = v7 `xor` (v7 `shiftL` 5)
+
+hash :: Int32 -> Int32
+hash x = fromIntegral (hashed `mod` bound) where
+  bound = fromIntegral (maxBound :: Int32)
+  hashed = hashWord (fromIntegral x)
